8c0f1f41ead3607bfd183a6c8948c92af930cd7d
Atif Hussain <atifhussain.nu21@gmail.com>
2016-08-10 13:01:56 +0500

    Android Development, Activity steps.

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    String msg = "Android : ";
+
+    /** Called when the activity is first created. */
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(msg, "The onCreate() event : step-1");
+    }
+
+    /** Called when the activity is about to become visible. */
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(msg, "The onStart() event : step-2");
+    }
+
+    /** Called when the activity has become visible. */
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(msg, "The onResume() event : step-3");
+    }
+
+    /** Called when another activity is taking focus. */
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(msg, "The onPause() event : step-4");
+    }
+
+    /** Called when the activity is no longer visible. */
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(msg, "The onStop() event : step-5");
+    }
+
+    /** Called just before the activity is destroyed. */
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(msg, "The onDestroy() event : step-6");
+    }
+}
