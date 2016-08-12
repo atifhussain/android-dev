ccd50a6bd212ed180a387e7097be5aeb53c6e38d
Mehtab Ghani <mehtab.ghani@tenpearls.com>
2016-08-12 17:09:13 +0500

    Fragment integrated, now can check both landscape and protait screens

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.myapplication;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
+import android.content.res.Configuration;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,7 +15,29 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        Configuration config = getResources().getConfiguration();
+
+        FragmentManager fragmentManager = getFragmentManager();
+        FragmentTransaction fragmentTransaction =
+                fragmentManager.beginTransaction();
+
+        /**
+         * Check the device orientation and act accordingly
+         */
+        if (config.orientation == Configuration.ORIENTATION_LANDSCAPE) {
+            /**
+             * Landscape mode of the device
+             */
+            LM_Fragement ls_fragment = new LM_Fragement();
+            fragmentTransaction.replace(android.R.id.content, ls_fragment);
+        }else{
+            /**
+             * Portrait mode of the device
+             */
+            PM_Fragement pm_fragment = new PM_Fragement();
+            fragmentTransaction.replace(android.R.id.content, pm_fragment);
+        }
+        fragmentTransaction.commit();
         Log.d(msg, "The onCreate() event : step-1");
     }
 
